4ade3bcbce8c78e1b76a5427cb1722d6a3c9bc38
80997 <809979314@qq.com>
2018-01-02 14:36:12 +0800

    代码改动

---
+++ app/src/main/java/chapter10_multithreading/coolweather/MainActivity.java
@@ -0,0 +1,13 @@
+package chapter10_multithreading.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
